943717e56e78b6bb5be9d1c397ebe9dc499eaac8
edoardo <omer2.it@hotmail.com>
2024-03-14 18:53:06 -0400

    Update utils.py

--- debug_python_code/utils.py
+++ debug_python_code/utils.py
@@ -9,10 +9,10 @@ def remove_mess(image, window_size=10, thresh=.4):
             roi = image[y:y+window_size, x:x+window_size]
             
             # Count white pixels 
-            white_pixels = np.sum(roi == [255, 255, 255])
+            white_pixels = np.sum(roi == 255)
             
             # If there are 20 or more white pixels, replace the entire ROI with black pixels
             if white_pixels >= pixels_threshold:
-                image[y:y+window_size, x:x+window_size] = [0, 0, 0]
+                image[y:y+window_size, x:x+window_size] = 0
                 # print("Removed patch")
     return image
