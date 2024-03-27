e8326200b55f44cce4457619ecffcc77be05d25a
Timdnb <timdnblanken@gmail.com>
2024-03-27 17:49:34 +0100

    Rename to waypoint version

--- sw/airborne/modules/cnn_guided/cnn_guided_waypoint.h
+++ sw/airborne/modules/cnn_guided/cnn_guided_waypoint.h
@@ -7,29 +7,29 @@
 /**
  * @file "modules/cnn_guided/cnn_guided.h"
  * @author Tim den Blanken
- * TO COME
+ * Header file for cnn guided module
  */
 
 #ifndef CNN_GUIDED_H
 #define CNN_GUIDED_H
 
-
 // Add the enum declaration here
 enum navigation_state_t {
-  PATH_FOLLOWING,
   FORWARD,
   LEFT,
   RIGHT,
   OUT_OF_BOUNDS,
   REENTER_ARENA,
-  STATIONARY
+  STATIONARY,
+  PATH_FOLLOWING
 };
 
 // settings
 extern float oag_max_speed;         // max flight speed [m/s]
 extern float oag_heading_rate;      // heading rate setpoint [rad/s]
+extern float base_speed_fraction;   // base_speed = oag_max_speed * base_speed_fraction
 
-// External declarations to access the confidence values
+// External declarations to access the confidence values for logger file
 extern float forward_conf;
 extern float right_conf;
 extern float left_conf; 
@@ -38,6 +38,5 @@ extern void cnn_guided_init(void);
 extern void cnn_guided_periodic(void);
 
 void update_navigation_state(float forward_conf, float right_conf, float left_conf);
-
 #endif /* CNN_GUIDED_H */
 
